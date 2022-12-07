6f8f265769e5a4a56661b79f671824bf965ec646
Sudarshan Patil <p.sdrshn@gmail.com>
2022-12-07 22:37:42 +0530

    login

--- index.php
+++ index.php
@@ -1,71 +1,84 @@
-* {
-	margin: 0px;
-	padding: 0px;
-}
-body {
-	font-size: 120%;
-	background: #F8F8FF;
+<?php
+
+// Starting the session, to use and
+// store data in session variable
+session_start();
+
+// If the session variable is empty, this
+// means the user is yet to login
+// User will be sent to 'login.php' page
+// to allow the user to login
+if (!isset($_SESSION['username'])) {
+	$_SESSION['msg'] = "You have to log in first";
+	header('location: login.php');
 }
 
-.header {
-	width: 30%;
-	margin: 50px auto 0px;
-	color: white;
-	background: #5F9EA0;
-	text-align: center;
-	border: 1px solid #B0C4DE;
-	border-bottom: none;
-	border-radius: 10px 10px 0px 0px;
-	padding: 20px;
+// Logout button will destroy the session, and
+// will unset the session variables
+// User will be headed to 'login.php'
+// after logging out
+if (isset($_GET['logout'])) {
+	session_destroy();
+	unset($_SESSION['username']);
+	header("location: login.php");
 }
-form, .content {
-	width: 30%;
-	margin: 0px auto;
-	padding: 20px;
-	border: 1px solid #B0C4DE;
-	background: white;
-	border-radius: 0px 0px 10px 10px;
-}
-.input-group {
-	margin: 10px 10px 10px 10px;
-}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage</title>
+	<link rel="stylesheet" type="text/css"
+					href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Home Page</h2>
+	</div>
+	<div class="content">
 
-.input-group label {
-	display: block;
-	text-align: left;
-	margin: 5px;
-	font-size: 20px;
-}
-.input-group input {
-	height: 32px;
-	width: 95%;
-	padding: 5px 10px;
-	font-size: 15px;
-	border-radius: 10px;
-	border: 1px solid gray;
-}
-.btn {
-	cursor: pointer;
-	padding: 12px;
-	font-size: 16px;
-	color: white;
-	background: #23585a;
-	border: none;
-	border-radius: 10px;
-}
-.error {
-	width: 92%;
-	margin: 0px auto;
-	padding: 10px;
-	border: 1px solid #a94442;
-	color: #a94442;
-	background: #f2dede;
-	border-radius: 5px;
-	text-align: left;
-}
-.success {
-	color: #3c763d;
-	background: #dff0d8;
-	border: 1px solid #3c763d;
-	margin-bottom: 20px;
-}
+		<!-- Creating notification when the
+				user logs in -->
+		
+		<!-- Accessible only to the users that
+				have logged in already -->
+		<?php if (isset($_SESSION['success'])) : ?>
+			<div class="error success" >
+				<h3>
+					<?php
+						echo $_SESSION['success'];
+						unset($_SESSION['success']);
+					?>
+				</h3>
+			</div>
+		<?php endif ?>
+
+		<!-- information of the user logged in -->
+		<!-- welcome message for the logged in user -->
+		<?php if (isset($_SESSION['username'])) : ?>
+			
+
+
+<p>
+				Welcome
+				<strong>
+					<?php echo $_SESSION['username']; ?>
+				</strong>
+			</p>
+
+
+
+			
+
+
+<p>
+				<a href="index.php?logout='1'" style="color: red;">
+					Click here to Logout
+				</a>
+			</p>
+
+
+
+		<?php endif ?>
+	</div>
+</body>
+</html>

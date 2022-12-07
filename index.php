81dc454410c432b8ac5d0afe28c572f7c9be50b0
Sudarshan Patil <p.sdrshn@gmail.com>
2022-12-07 22:47:29 +0530

    updae

--- index.php
+++ index.php
@@ -1,34 +1,27 @@
-<?php
+<?php 
+	session_start(); //starting the session, to use and store data in session variable
 
-// Starting the session, to use and
-// store data in session variable
-session_start();
+	//if the session variable is empty, this means the user is yet to login
+	//user will be sent to 'login.php' page to allow the user to login
+	if (!isset($_SESSION['username'])) {
+		$_SESSION['msg'] = "You have to log in first";
+		header('location: login.php');
+	}
 
-// If the session variable is empty, this
-// means the user is yet to login
-// User will be sent to 'login.php' page
-// to allow the user to login
-if (!isset($_SESSION['username'])) {
-	$_SESSION['msg'] = "You have to log in first";
-	header('location: login.php');
-}
+	// logout button will destroy the session, and will unset the session variables
+	//user will be headed to 'login.php' after loggin out
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['username']);
+		header("location: login.php");
+	}
 
-// Logout button will destroy the session, and
-// will unset the session variables
-// User will be headed to 'login.php'
-// after logging out
-if (isset($_GET['logout'])) {
-	session_destroy();
-	unset($_SESSION['username']);
-	header("location: login.php");
-}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Homepage</title>
-	<link rel="stylesheet" type="text/css"
-					href="style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 	<div class="header">
@@ -36,16 +29,13 @@ if (isset($_GET['logout'])) {
 	</div>
 	<div class="content">
 
-		<!-- Creating notification when the
-				user logs in -->
-		
-		<!-- Accessible only to the users that
-				have logged in already -->
+		<!-- creating notification when the user logs in -->
+		<!-- accessible only to the users that have logged in already -->
 		<?php if (isset($_SESSION['success'])) : ?>
 			<div class="error success" >
 				<h3>
-					<?php
-						echo $_SESSION['success'];
+					<?php 
+						echo $_SESSION['success']; 
 						unset($_SESSION['success']);
 					?>
 				</h3>
@@ -54,31 +44,11 @@ if (isset($_GET['logout'])) {
 
 		<!-- information of the user logged in -->
 		<!-- welcome message for the logged in user -->
-		<?php if (isset($_SESSION['username'])) : ?>
-			
-
-
-<p>
-				Welcome
-				<strong>
-					<?php echo $_SESSION['username']; ?>
-				</strong>
-			</p>
-
-
-
-			
-
-
-<p>
-				<a href="index.php?logout='1'" style="color: red;">
-					Click here to Logout
-				</a>
-			</p>
-
-
-
+		<?php  if (isset($_SESSION['username'])) : ?>
+			<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+			<p> <a href="index.php?logout='1'" style="color: red;">Click here to Logout</a> </p>
 		<?php endif ?>
 	</div>
+		
 </body>
 </html>
--- register.php
+++ register.php
@@ -2,13 +2,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>
-		Registration system PHP and MySQL
-	</title>
-	<link rel="stylesheet" type="text/css"
-					href="style.css">
+	<title>Registration system PHP and MySQL</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-
 <body>
 	<div class="header">
 		<h2>Register</h2>
@@ -20,13 +16,11 @@
 
 		<div class="input-group">
 			<label>Enter Username</label>
-			<input type="text" name="username"
-				value="<?php echo $username; ?>">
+			<input type="text" name="username" value="<?php echo $username; ?>">
 		</div>
 		<div class="input-group">
 			<label>Email</label>
-			<input type="email" name="email"
-				value="<?php echo $email; ?>">
+			<input type="email" name="email" value="<?php echo $email; ?>">
 		</div>
 		<div class="input-group">
 			<label>Enter Password</label>
@@ -37,23 +31,11 @@
 			<input type="password" name="password_2">
 		</div>
 		<div class="input-group">
-			<button type="submit" class="btn"
-								name="reg_user">
-				Register
-			</button>
+			<button type="submit" class="btn" name="reg_user">Register</button>
 		</div>
-		
-
-
-<p>
-			Already having an account?
-			<a href="login.php">
-				Login Here!
-			</a>
+		<p>
+			Already having an account? <a href="login.php">Login Here!</a>
 		</p>
-
-
-
 	</form>
 </body>
 </html>

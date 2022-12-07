0333432cbb4743d324a609ac2bfa38912b8ab426
Sudarshan Patil <p.sdrshn@gmail.com>
2022-12-07 23:15:08 +0530

    set blank

--- Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost:3036');
-   define('DB_USERNAME', 'simpleco_sdrshnptl');
-   define('DB_PASSWORD', 'W2S[^U%k{k@E');
-   define('DB_DATABASE', 'registration');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>

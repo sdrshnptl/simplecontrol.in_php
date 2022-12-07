7e00c632d9b0e9a4eaee7b5ca451a8078a1a50d4
Sudarshan Patil <p.sdrshn@gmail.com>
2022-12-07 22:45:26 +0530

    php

--- error.php
+++ error.php
@@ -1,13 +1,13 @@
-<?php if (count($errors) > 0) : ?>
-	<div class="error">
-		<?php foreach ($errors as $error) : ?>
-		
-
-
+<?php  if (count($errors) > 0) : ?>
+    <div class="error">
+        <?php foreach ($errors as $error) : ?>
+         
+ 
+ 
 <p><?php echo $error ?></p>
-
-
-
-		<?php endforeach ?>
-	</div>
-<?php endif ?>
+ 
+ 
+ 
+        <?php endforeach ?>
+    </div>
+<?php  endif ?>
